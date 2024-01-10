43a39d551abbfe518bddd9d73d437f8a6f8fb09b
Shpe3 <90975505+Shpe3@users.noreply.github.com>
2024-01-10 03:02:48 +0300

    I'm idiot

---
+++ server/src/main/java/server/commands/Executable.java
@@ -0,0 +1,16 @@
+package server.commands;
+
+import common.network.requests.Request;
+import common.network.responses.Response;
+
+/**
+ * Интерфейс для всех выполняемых команд.
+ */
+public interface Executable {
+  /**
+   * Выполнить что-либо.
+   * @param request запрос с данными для выполнения команды
+   * @return результат выполнения
+   */
+  Response apply(Request request);
+}
